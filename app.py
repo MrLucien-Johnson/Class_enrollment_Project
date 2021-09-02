990114eb2447ca57d0b07b2e5cf6d4d661ae6d7f
Abel <al14aac@herts.ac.uk>
2021-09-02 14:55:34 +0100

    changes to app py setup database starting

--- app.py
+++ app.py
@@ -1,10 +1,20 @@
-from flask import Flask
+from flask import *
 
 app = Flask(__name__)
 
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://user:password@123.45.6.78:3306/class_enrollment_Project'
+
 @app.route('/')
-def hello_internet():
-    return "Hello Internet!"
+def no_url():
+    return  redirect('/home')
+
+@app.route('/home')
+def home_url():
+    return  "Home for future students"
+
+@app.route('/enroll')
+def enroll_url():
+    return  "Hello future students"
 
 if __name__=='__main__':
     app.run(debug=True, host='0.0.0.0')
