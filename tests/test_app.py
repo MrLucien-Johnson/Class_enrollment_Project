ffaf64b5801c02f760ad910ede5526f1c174d34e
Ubuntu <ubuntu@ip-172-31-14-12.eu-west-1.compute.internal>
2021-09-10 12:02:25 +0000

    added test cases

--- tests/test_app.py
+++ tests/test_app.py
@@ -32,7 +32,7 @@ def setUp(self):
         db.session.add(s1)
         db.session.commit()
 
-        c1 = Classes(classes_name = 'friday', classes_day = "monday", status = 'correct')
+        c1 = Classes(classes_name = 'friday', classes_day = "monday")
         db.session.add(c1)
         db.session.commit()
 
@@ -56,14 +56,41 @@ class TestViewHome(TestBase):
         self.assertEqual(response.status_code, 200)
         self.assertIn(b'HOME', response.data)  
 
-class TestViewAddStudent(TestBase):
+class TestViewGetAddStudent(TestBase):
     def test_addstudent_get(self):
         response = self.client.get(url_for('addStudent'))
         self.assertEqual(response.status_code, 200)
         self.assertIn(b'Add Student', response.data)    
 
-class TestViewAddClasses(TestBase):
+class TestViewGetAddClasses(TestBase):
     def test_addclasses_get(self):
         response = self.client.get(url_for('addClasses'))
         self.assertEqual(response.status_code, 200)
-        self.assertIn(b'Add classes', response.data) 
+        self.assertIn(b'Add Classes', response.data) 
+
+class TestAddClasses(TestBase):
+    def test_add_Classes(self):
+        response = self.client.post(
+            url_for('addClasses'),
+            data = dict(classes_name="friday", classes_day = "monday"),
+            follow_redirects=True
+        )
+        self.assertIn(b'Add Classes',response.data)
+
+class TestAddStudent(TestBase):
+    def test_add_Student(self):
+        response = self.client.post(
+            url_for('addStudent'),
+            data = dict(studnet_name="testadd",student_age=26 , student_city="London"),
+            follow_redirects=True
+        )
+        self.assertIn(b'Add Student',response.data)
+
+class TestUpdateStudent(TestBase):
+    def test_update_student(self):
+        response = self.client.post(
+            url_for('updateStudent', qid=1),
+            data = dict(studnet_name="testupdate",student_age=30 , student_city="Update"),
+            follow_redirects=True
+        )
+        self.assertIn(b'Home',response.data)
